1c2d3992b84d260a5b3760331634a99f09e8eda5
Jose Antonio Mora <j_antonio_mora@hotmail.com>
2020-09-29 16:51:33 -0500

    Primer Commit docs

---
+++ docs/precache-manifest.5b3f991fb73ad4e35fa97079e30482ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643037cf081abdfaad398a0ac0552a3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0c36db8c700bcf99b9b",
+    "url": "/static/css/main.7f466788.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c36db8c700bcf99b9b",
+    "url": "/static/js/main.3e58f65d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
